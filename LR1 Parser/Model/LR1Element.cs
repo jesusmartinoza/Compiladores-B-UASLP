57bb6c6db5cf909e69fb5d32e319e818aa4bfeac
Jesús Alberto Martínez Mendoza <hongo-loco@hotmail.com>
2019-03-12 10:43:16 -0600

    Se crean clases y estructuras de datos generales del proyecto, .gitignore agregado

---
+++ LR1 Parser/Model/LR1Element.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1_Parser.Model
+{
+    class LR1Element
+    {
+        List<Token> alpha; // Conjunto de tokens del lado izquierdo del punto
+        List<Token> gama; // Conjunto de tokens del lado derecho del punto
+    }
+}
---
+++ LR1 Parser/Model/State.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1_Parser.Model
+{
+    /// <summary>  
+    /// Representa un estado en la tabla de análisis sintáctico.
+    /// 
+    /// Contiene dos diccionarios para terminales y no terminales, indexados por el token en forma de string.
+    /// </summary> 
+    class State
+    {
+        Dictionary<string, Action> nonTerminals; // Conjunto de acciones de los no terminales
+        Dictionary<string, Action> terminals; // Conjunto de acciones de los terminales
+    }
+
+    /// <summary>  
+    /// Representa una acción, por ejemplo s2, r1, etc...
+    /// 
+    /// La acción puede tener los valores:
+    /// S which means SHIFT
+    /// R which means REDUX
+    /// A which means ACCEPT
+    /// </summary> 
+    struct Action
+    {
+        char action; // Puede ser 'S', 'R' u 'A'
+        int state; // Estado al que va
+    }
+}
