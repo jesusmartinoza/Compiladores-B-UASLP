b676b0e8685f56e3688cc97116114cb13f14bb04
josedejesusAmaya <pp.jjag@gmail.com>
2019-03-18 16:30:31 -0600

    Update Tokenizer.cs

--- LR1 Parser/Tokenizer.cs
+++ LR1 Parser/Tokenizer.cs
@@ -12,7 +12,36 @@ namespace LR1_Parser.Model
     /// 
     /// Módulo desarrollado por Chivas :D
     /// </summary>
-    class Tokenizer
+    public class Token
     {
+        string content;
+        bool terminal; // Bandera para indicar si es terminal o no-terminal
+        string val; // Guarda lexema o el resultado de evaluar un conjunto de atributos
+
+        public Token(string s, bool t)
+        {
+            terminal = t;
+            content = s;
+        }
+
+        //Getters and Setters
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+        public bool Terminal
+        {
+            get { return terminal; }
+            set { terminal = value; }
+        }
+
+        public string Val
+        {
+            get { return val; }
+            set { val = value; }
+        }
+
     }
 }
