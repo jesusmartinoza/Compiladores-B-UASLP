f2069b8c701257efd1b96086d78bd67fc6e0d686
josedejesusAmaya <pp.jjag@gmail.com>
2019-03-18 16:52:50 -0600

    Update Production.cs

--- LR1 Parser/Model/Production.cs
+++ LR1 Parser/Model/Production.cs
@@ -8,7 +8,7 @@ namespace LR1_Parser.Model
 {
     class Production
     {
-        string id;
+        /*string id;
         List<Token> left; // Conjunto de tokens del lado izquierdo de la flecha 
         List<Token> right;  // Conjunto de tokens del lado derecho de la flecha 
         HashSet<Token> first; // Conjunto de primeros de forma desordenada
@@ -16,6 +16,41 @@ namespace LR1_Parser.Model
         public string Id { get => id; set => id = value; }
         internal List<Token> Left { get => left; set => left = value; }
         internal List<Token> Right { get => right; set => right = value; }
-        internal HashSet<Token> First { get => first; set => first = value; }
+        internal HashSet<Token> First { get => first; set => first = value; }*/
+        int id;
+        Token left; // Conjunto de tokens del lado izquierdo de la flecha 
+        List<Token> right;  // Conjunto de tokens del lado derecho de la flecha 
+        HashSet<Token> first; // Conjunto de primeros de forma desordenada
+
+        public Productions(int i, Token l){
+            id = i;
+            left = l;
+            right = new List<Token>();
+        }
+
+        //Getters and Setters
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        internal Token Left
+        {
+            get { return left; }
+            set { left = value; }
+        }
+
+        internal List<Token> Right
+        {
+            get { return right; }
+            set { right = value; }
+        }
+
+        internal HashSet<Token> First
+        {
+            get { return first; }
+            set { first = value; }
+        }
     }
 }
