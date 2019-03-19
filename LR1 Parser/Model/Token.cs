482e55c6320e215dd4b09b25a34c1d4f28033811
josedejesusAmaya <pp.jjag@gmail.com>
2019-03-18 18:30:18 -0600

    Update Token.cs

--- LR1 Parser/Model/Token.cs
+++ LR1 Parser/Model/Token.cs
@@ -8,13 +8,40 @@ namespace LR1_Parser.Model
 {
     class Token
     {
-        string content;
+        /*string content;
         bool terminal; // Bandera para indicar si es terminal o no-terminal
         string val; // Guarda lexema o el resultado de evaluar un conjunto de atributos
         //Getters and Setters
         public string Content { get => content; set => content = value; }
         public bool Terminal { get => terminal; set => terminal = value; }
-        public string Val { get => val; set => val = value; }
+        public string Val { get => val; set => val = value; }*/
+        string content;
+        bool terminal; // Bandera para indicar si es terminal o no-terminal
+        string val; // Guarda lexema o el resultado de evaluar un conjunto de atributos
 
+        public Token(string s, bool t)
+        {
+            terminal = t;
+            content = s;
+        }
+
+        //Getters and Setters
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+        public bool Terminal
+        {
+            get { return terminal; }
+            set { terminal = value; }
+        }
+
+        public string Val
+        {
+            get { return val; }
+            set { val = value; }
+        }
     }
 }
