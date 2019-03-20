41fe801bc5104d13c4fcc6e0cfe9ca92cc8d6425
AlfredoGJ <josalfk@hotmail.com>
2019-03-19 22:01:58 -0600

    Cambios para poder fusionar

--- LR1 Parser/Views/PageGramatica.xaml.cs
+++ LR1 Parser/Views/PageGramatica.xaml.cs
@@ -78,68 +78,7 @@ namespace LR1_Parser
 
         private void GenerarTabla_Click(object sender, RoutedEventArgs e)
         {
-            ////Inicializacion de variables
-            //producciones = new List<Production>();
-            //tokenNT = new List<string>();
-            //tokenT = new List<string>();
-            //tokens = new List<Token>();
-            ////TText.Text = NTtext.Text = "";
-            //indice = 1;
-            //List<string> grammar = new List<string>();
-            //string aux = EntradaGramatica.Text.Replace("\r", "");
-            //aux = aux.Replace("->", "→");
-            //grammar = aux.Split('\n').ToList();
-
-            ////Separacion en NT y T
-            //foreach (string s in grammar)
-            //{
-            //    if (!tokenNT.Contains(s.Split('→')[0]))//Verifica que no exista ya en la lista
-            //    {
-            //        tokenNT.Add(s.Split('→')[0]);
-            //        tokens.Add(new Token(s.Split('→')[0], false));
-            //    }
-            //}
-            //foreach (string s in grammar)
-            //{
-            //    foreach (string ss in s.Split('→')[1].Split(' '))//Separa los diferentes tokens de la parte derecha de la flecha
-            //    {
-            //        if (!tokenNT.Contains(ss) && !tokenT.Contains(ss) && ss != "|")//Verifica que no sea un NT, aun no exista en la lista o sea el operador "|" de las gramaticas
-            //        {
-            //            tokenT.Add(ss);
-            //            tokens.Add(new Token(ss, true));
-            //        }
-            //    }
-            //}
-
-            //foreach (string s in grammar)
-            //{
-            //    string[] aux2 = s.Split('→');
-            //    List<string> list = aux2[1].Split('|').ToList();
-
-            //    foreach (string ss in list)
-            //    {
-            //        producciones.Add(new Production(indice, getToken(aux2[0])));
-            //        List<string> tokenProd = ss.Split(' ').ToList();
-            //        foreach (string ss2 in tokenProd)
-            //        {
-            //            if (ss2 != "")
-            //            {
-            //                producciones[producciones.Count - 1].Right.Add(getToken(ss2));
-            //            }
-            //        }
-            //        indice++;
-            //    }
-            //}
-
-            ////Muestra en pantalla
-            //foreach (string s in tokenNT)
-            //{
-            //    //NTtext.Text += s + "\r\n";
-            //}
-            //foreach (string s in tokenT)
-            //{
-            //    //TText.Text += s + "\r\n";
-            //}
+            
 
             TestPrimeros();
 
