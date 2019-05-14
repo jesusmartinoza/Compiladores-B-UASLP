a27f0c5ef264997c962ba939e4698f461d4d4339
AlfredoGJ <josalfk@hotmail.com>
2019-05-14 01:28:01 -0500

    Inicializacion Clases para generar cuadruplos

---
+++ LR1 Parser/Model/QuadGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1_Parser.Model
+{
+    class QuadGenerator
+    {
+        delegate BinaryTreeNode Instruct2Quads(BinaryTreeNode node);
+        static List<Quad> Quads;
+        static Stack<Quad> ReturnsStack;
+        static Stack<object> AuxValuesStack;
+        static Dictionary<string, Instruct2Quads> Dictionary;
+
+        public static List<Quad> Generate(BinaryTreeNode tree)
+        {
+
+
+
+
+
+            return Quads;
+        }
+
+
+       
+
+        public static void SentWhile()
+        {
+
+        }
+
+
+
+
+    }
+
+    public struct Quad
+    {
+        string Operator;
+        object OperandA;
+        object OperandB;
+        object Result;
+
+    }
+}
