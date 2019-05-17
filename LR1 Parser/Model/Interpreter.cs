6885f76fae50218d43f2e5072c98cec9f515d076
Carlos <kazh027@gmail.com>
2019-05-17 12:08:02 -0500

    Switchazo creado y Quads Test

---
+++ LR1 Parser/Model/Interpreter.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1_Parser.Model
+{
+    class Interpreter
+    {
+        Dictionary<string, dynamic> simbTable;
+        List<Quad> quadsList;
+
+        public struct FormItem
+        {
+            public string id;
+            public int posX, posY, SizeX, SizeY;
+
+            public void SetID(string id)
+            {
+                this.id = id;
+            }
+
+            public void SetPosition(int x, int y)
+            {
+                posX = x;
+                posY = y;
+            }
+
+            public void SetSize(int x, int y)
+            {
+                SizeX = x;
+                SizeY = y;
+            }
+
+        }
+
+        public Interpreter()
+        {
+            GenerateManualQuads();
+            ReadByQuads();
+        }
+
+        private void GenerateManualQuads()
+        {
+            quadsList.Add(new Quad(":=",    "2000",     null,   "numero"));
+            quadsList.Add(new Quad(":=",    "0",        null,   "dato"));
+            quadsList.Add(new Quad(">",     "numero",   "1000", "t1"));
+            quadsList.Add(new Quad("gotoF", "t1",       "10",   null));
+            quadsList.Add(new Quad("-",     "numero",   "1",    "t2"));
+            quadsList.Add(new Quad(":=",    "t2",       null,   "numero"));
+            quadsList.Add(new Quad("+",     "dato",     "1",    "t3"));
+            quadsList.Add(new Quad(":=",    "t3",       null,   "dato"));
+            quadsList.Add(new Quad("goto",  "3",        null,   null));
+            quadsList.Add(new Quad("end",   null,       null,   null));
+        }
+
+        private void ReadByQuads()
+        {
+            for (int i = 0; i < quadsList.Count; i++)
+            {
+                switch(quadsList[i].Operator)
+                {
+                    case ":=":
+                        break;
+                    case "<":
+                        break;
+                    case "<=":
+                        break;
+                    case ">":
+                        break;
+                    case ">=":
+                        break;
+                    case "+":
+                        break;
+                    case "-":
+                        break;
+                    case "*":
+                        break;
+                    case "/":
+                        break;
+                    case "^":
+                        //checar si es menor de 0 la potencia, es una raiz cuadrada
+                        break;
+                    case "goto":
+                        break;
+                    case "gotoF":
+                        break;
+                    case "gotoT":
+                        break;
+                    case "idV":
+                        break;
+                    case "posV":
+                        break;
+                    case "tamV":
+                        break;
+                    case "idB":
+                        break;
+                    case "posB":
+                        break;
+                    case "tamB":
+                        break;
+                    case "idL":
+                        break;
+                    case "posL":
+                        break;
+                }
+            }
+        }
+    }
+}
